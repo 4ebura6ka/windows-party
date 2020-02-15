ef01bb38abe3472dd28ea6dee71cd41bde34cfcf
Serzas P <slipik@inbox.ru>
2020-02-15 11:20:54 +0200

    Caliburn.Micro included into solution's project.

---
+++ WpfServers/WpfUi/ViewModels/ServersViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace WpfUi.ViewModels
+{
+    public class ServersViewModel : Screen
+    {
+        private string login = "Serz";
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+            set
+            {
+                login = value;
+                NotifyOfPropertyChange(() => Login);
+            }
+        }
+    }
+}
--- WpfServers/WpfUi/Views/ServersView.xaml.cs
+++ WpfServers/WpfUi/Views/ServersView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfUi
+namespace WpfUi.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ServersView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ServersView : Window
     {
-        public MainWindow()
+        public ServersView()
         {
             InitializeComponent();
         }
